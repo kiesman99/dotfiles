ddf825de46b90b07b1f2dd0f74890bb89b28c8c0
Justin <kiesman@pop-os.localdomain>
2020-01-07 20:53:36 +0100

    Added some aliases
    Added colorls aliases
    Added vim alias to enforce nvim

--- aliases.sh
+++ aliases.sh
@@ -1,1 +1,7 @@
 alias clip='xclip -selection c'
+alias vim='echo "Please use nvim."'
+# colorls alias
+alias ols='/usr/bin/ls'
+alias oll='/usr/bin/ls -lh'
+alias ls='colorls'
+alias ll='colorls -lh'
