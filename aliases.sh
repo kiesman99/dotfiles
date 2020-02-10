fb3ae4fb4d23fb4b3d8e5be64df68aea168cc5e9
Justin V <vietz@uni-bremen.de>
2020-02-10 06:52:18 +0100

    Added some aliases for apt.

--- aliases.sh
+++ aliases.sh
@@ -5,3 +5,11 @@ alias ols='/usr/bin/ls'
 alias oll='/usr/bin/ls -lh'
 alias ls='colorls'
 alias ll='colorls -lh'
+
+# just some helpful things
+alias cc='clear'
+
+# apt aliases
+alias apts='apt search'
+alias apti='sudo apt install'
+alias aptr='sudo apt remove'
