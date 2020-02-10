8c99de7b0a05f8c4e90baa09b6d77ac161cb7802
Justin V <vietz@uni-bremen.de>
2020-02-10 06:53:13 +0100

    Added some export PATHS.
    Dart
    SK
    pub cache

--- exports.sh
+++ exports.sh
@@ -1,10 +1,23 @@
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 export PATH="$PATH:/Users/justinvietz/sdks/flutter/bin:$Home/bin:/usr/local/bin"
+# Dart SDK
+# export PATH="$PATH:/home/kiesman/.sdks/flutter/bin/cache/dart-sdk/bin"
+export PATH="$PATH:/usr/lib/dart/bin"
+# Dart Pub Executables
+export PATH="$PATH":"$HOME/.pub-cache/bin"
 
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
 
+# add skim to path
+export PATH="$PATH:$HOME/.skim/bin"
+
+# export rust
+export PATH="$PATH:$HOME/.cargo/bin"
+
+
+export JAVA_HOME="/usr/lib/jvm/java-13-openjdk-amd64"
 
 
 export VISUAL=nvim
